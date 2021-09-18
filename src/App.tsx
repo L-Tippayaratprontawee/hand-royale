a16da9205b4387bff1030abde42de23afa28c26b
L Tippayaratprontawee <atip854@aucklanduni.ac.nz>
2021-09-19 04:34:29 +1200

    Added Avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React, { useState } from 'react'
-import { Button } from 'antd'
+import { Avatar, Button } from 'antd'
 import 'antd/dist/antd.css'
 import styles from './App.module.css'
+import { UserOutlined } from '@ant-design/icons'
 
 function App() {
     const handSelection = ['PAPER', 'SCISSORS', 'ROCK']
@@ -13,26 +14,26 @@ function App() {
     const validateMatch = (player1Hand: string) => {
         const randomIndex = Math.floor(Math.random() * handSelection.length)
         const player2Hand = handSelection[randomIndex]
-        ///console.log({ player2Hand })
+        console.log({ player2Hand })
         if (player1Hand === player2Hand) {
-            setMatchResult("It's a tie!")
+            setMatchResult('You both choose ' + player1Hand + ". IT'S A DRAW")
         } else if (player1Hand === 'PAPER') {
             if (player2Hand === 'ROCK') {
-                setMatchResult('YOU WIN!')
+                setMatchResult('Paper wraps Rock. YOU WIN!')
             } else {
-                setMatchResult('YOU LOSE!')
+                setMatchResult('Scissors cut Paper. YOU LOSE!')
             }
         } else if (player1Hand === 'SCISSORS') {
             if (player2Hand === 'PAPER') {
-                setMatchResult('YOU WIN!')
+                setMatchResult('Scissors cut Paper. YOU WIN!')
             } else {
-                setMatchResult('YOU LOSE!')
+                setMatchResult('Rock destroys Scissors. YOU LOSE!')
             }
         } else if (player1Hand === 'ROCK') {
             if (player2Hand === 'SCISSORS') {
-                setMatchResult('YOU WIN!')
+                setMatchResult('Rock destroys Scissors. YOU WIN!')
             } else {
-                setMatchResult('YOU LOSE!')
+                setMatchResult('Paper wraps Rock. YOU LOSE!')
             }
         }
     }
@@ -47,6 +48,16 @@ function App() {
             }}
         >
             <h1>{matchResult}</h1>
+
+            <div>
+                <Avatar
+                    shape="circle"
+                    size={64}
+                    icon={<UserOutlined />}
+                    src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fc26be893-1c94-48a3-b3e1-6e2c7409f07e%2FIMG_8825.jpg?table=block&id=6da5bea8-5fac-4bca-87ec-1ee329fe691c&spaceId=9c5958aa-96c4-4ffb-853d-fd1c1b2a4a4f&width=250&userId=358dff24-baa1-4893-8192-e15f1f93689c&cache=v2"
+                />
+                <p>NAME</p>
+            </div>
 
             <Button type="primary" size={size} className={styles.button} onClick={() => validateMatch('PAPER')}>
                 PAPER
