723e3985aaece322e2d5edde951bb3df7866a77d
L Tippayaratprontawee <atip854@aucklanduni.ac.nz>
2021-09-19 03:31:10 +1200

    Get players action

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,38 @@ import 'antd/dist/antd.css'
 import styles from './App.module.css'
 
 function App() {
-    const player2Hand = 'ROCK'
+    const handSelection = ['PAPER', 'SCISSORS', 'ROCK']
+    console.log(handSelection.length)
+    const randomIndex = Math.floor(Math.random() * handSelection.length)
+    const player2Hand = handSelection[randomIndex]
+    console.log('player 2 hand', { player2Hand })
+    ///const player2Hand = 'PAPER'
     ///const [player1Hand, setPlayer1Hand] = useState('')
     const [matchResult, setMatchResult] = useState('')
     const validateMatch = (player1Hand: string) => {
         if (player1Hand === player2Hand) {
-            setMatchResult('DRAW!')
-        } else if (player1Hand === 'PAPER') {
-            setMatchResult('WIN!')
-        } else {
-            setMatchResult('LOSE!')
+            setMatchResult("It's a tie!")
+        } else if (player2Hand === 'PAPER') {
+            if (player1Hand === 'ROCK') {
+                setMatchResult('YOU WIN!')
+            } else {
+                setMatchResult('YOU LOSE!')
+            }
+        } else if (player2Hand === 'SCISSORS') {
+            if (player1Hand === 'PAPER') {
+                setMatchResult('YOU WIN!')
+            } else {
+                setMatchResult('YOU LOSE!')
+            }
+        } else if (player2Hand === 'ROCK') {
+            if (player1Hand === 'SCISSORS') {
+                setMatchResult('YOU WIN!')
+            } else {
+                setMatchResult('YOU LOSE!')
+            }
         }
     }
+
     const size = 'large'
     return (
         <div
