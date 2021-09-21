e4b8f1cadebded57f164e513d0904ea470e439cc
L Tippayaratprontawee <atip854@aucklanduni.ac.nz>
2021-09-22 01:18:16 +1200

    Created BrowserRouth, Added Re-fetch API

---
+++ src/components/History.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../App.module.scss'
+
+export const MatchHistory = () => {
+    return (
+        <div className={styles.container}>
+            <h1> This is your match history </h1>
+        </div>
+    )
+}
