16302bc9fd004cdfad627f7a53da97eb3c8b39af
L Tippayaratprontawee <atip854@aucklanduni.ac.nz>
2021-09-23 19:07:16 +1200

    Added two tables to Azure database

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -3,15 +3,16 @@ import { gql, useQuery } from '@apollo/client'
 // import styles from '../App.module.scss'
 
 export const MatchHistory = () => {
-    const EXCHANGE_RATES = gql`
-        query GetExchangeRates {
-            rates(currency: "USD") {
-                currency
-                rate
+    const MATCH_HISTORY = gql`
+        query GetMatchHistory {
+            matches {
+                player {
+                    name
+                }
             }
         }
     `
-    const { loading, error, data } = useQuery(EXCHANGE_RATES)
+    const { loading, error, data } = useQuery(MATCH_HISTORY)
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error :(</p>
 
