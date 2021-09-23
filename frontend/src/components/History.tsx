ef680cee766266f526e444d97e6ed65426ff6cb1
L Tippayaratprontawee <atip854@aucklanduni.ac.nz>
2021-09-24 01:20:44 +1200

    Showed match history page

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { gql, useQuery } from '@apollo/client'
-import { Table } from 'antd'
+import { Avatar } from 'antd'
 
 export const MatchHistory = () => {
     const MATCH_HISTORY = gql`
@@ -15,34 +15,34 @@ export const MatchHistory = () => {
             }
         }
     `
-    // type MatchHistoryProps = {
-    //     id: number
-    //     result: string
-    //     player: {
-    //         name: string
-    //         imageURL: string
-    //     }
-    // }
+    type MatchHistoryProps = {
+        id: number
+        result: string
+        player: {
+            name: string
+            imageURL: string
+        }
+    }
     const { loading, error, data } = useQuery(MATCH_HISTORY)
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error :(</p>
 
-    // return data.matches.map(({ id, result, player }: MatchHistoryProps) => (
-    //     <div key={id}>
-    //         <p>
-    //             <Avatar shape="circle" size={50} src={player.imageURL} />
-    //             You {result} against {player.name}
-    //         </p>
-    //     </div>
-    // ))
+    return data.matches.map(({ id, result, player }: MatchHistoryProps) => (
+        <div key={id}>
+            <p>
+                <Avatar shape="circle" size={50} src={player.imageURL} />
+                You {result} against {player.name}
+            </p>
+        </div>
+    ))
 
-    const columns = [
-        {
-            title: 'Result',
-            dataIndex: 'result',
-            key: 'result',
-        },
-    ]
+    // const columns = [
+    //     {
+    //         title: 'Result',
+    //         dataIndex: 'result',
+    //         key: 'result',
+    //     },
+    // ]
 
-    return <Table columns={columns} dataSource={data.matches} />
+    // return <Table columns={columns} dataSource={data.matches} />
 }
