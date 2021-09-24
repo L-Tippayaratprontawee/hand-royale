aca29633abc2e41aed8589737ce612c43dde7f23
L Tippayaratprontawee <atip854@aucklanduni.ac.nz>
2021-09-24 23:16:37 +1200

    Finished polishing demo for MSA presentation

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { gql, useQuery } from '@apollo/client'
 import { Avatar, Table } from 'antd'
-//import styles from '../App.module.scss'
 
 export const MatchHistory = () => {
     const MATCH_HISTORY = gql`
@@ -15,14 +14,7 @@ export const MatchHistory = () => {
         }
     `
 
-    // type MatchHistoryProps = {
-    //     id: number
-    //     result: string
-    //     opponentName: string
-    //     opponentImageURL: string
-    // }
-
-    const { loading, error, data } = useQuery(MATCH_HISTORY)
+    const { loading, error, data } = useQuery(MATCH_HISTORY, { fetchPolicy: 'network-only' })
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error :(</p>
 
@@ -31,6 +23,18 @@ export const MatchHistory = () => {
             title: '',
             dataIndex: 'opponentImageURL',
             key: 'opponentImageURL',
+            render: (opponentImageURL: string) => (
+                <div>
+                    <Avatar
+                        shape="circle"
+                        size={50}
+                        style={{ margin: '5px' }}
+                        src="https://scontent.fakl4-1.fna.fbcdn.net/v/t1.6435-9/240873839_4240953529353032_4313338147101475536_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=WYlKF4kcK8EAX_qaRB_&tn=DKCNYcCS9aa7OF0L&_nc_ht=scontent.fakl4-1.fna&oh=fe93d5490cdbedf6a3b6c1b89fa722b5&oe=616BC865"
+                    />
+                    <b>vs</b>
+                    <Avatar shape="circle" size={50} src={opponentImageURL} style={{ margin: '5px' }} />
+                </div>
+            ),
         },
         {
             title: 'Result',
@@ -43,22 +47,5 @@ export const MatchHistory = () => {
             key: 'opponentName',
         },
     ]
-    return <Table columns={columns} dataSource={data.matches} />
-
-    // return data.matches.map(({ id, result, opponentName, opponentImageURL }: MatchHistoryProps) => (
-    //     <div key={id} className={styles.container}>
-    //         <p>
-    //             <Avatar
-    //                 shape="circle"
-    //                 size={50}
-    //                 src={
-    //                     'https://scontent.fakl4-1.fna.fbcdn.net/v/t1.6435-9/240873839_4240953529353032_4313338147101475536_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=WYlKF4kcK8EAX_qaRB_&tn=DKCNYcCS9aa7OF0L&_nc_ht=scontent.fakl4-1.fna&oh=fe93d5490cdbedf6a3b6c1b89fa722b5&oe=616BC865'
-    //                 }
-    //             />
-    //             VS.
-    //             <Avatar shape="circle" size={50} src={opponentImageURL} />
-    //             You {result} against {opponentName}
-    //         </p>
-    //     </div>
-    // ))
+    return <Table columns={columns} dataSource={data.matches} size="small" />
 }
