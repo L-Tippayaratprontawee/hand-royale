8d39dc234a2a43c84b79c9cdb1c74cec17abd0c5
L Tippayaratprontawee <atip854@aucklanduni.ac.nz>
2021-09-24 20:16:48 +1200

    Added table for match history

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,14 +5,18 @@ import styles from './Header.module.scss'
 
 export const Header = () => {
     return (
-        <div className={styles.container}>
-            <Button className={styles.button}>
-                <Link to="/">Battle Field</Link>
-            </Button>
-            <img src="logo3.png" className={styles.logo} />
-            <Button className={styles.button}>
-                <Link to="/match-history">Match History</Link>
-            </Button>
+        <div>
+            <div className={styles.container}>
+                <img src="logo3.png" className={styles.logo} />
+            </div>
+            <div className={styles.container}>
+                <Button className={styles.button}>
+                    <Link to="/">Battle Field</Link>
+                </Button>
+                <Button className={styles.button}>
+                    <Link to="/match-history">Match History</Link>
+                </Button>
+            </div>
         </div>
     )
 }
